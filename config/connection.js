2efd0cc5824e31613f5554a236a8f6fdaaf9c8f7
jwade4207 <jwade4207@yahoo.com>
2021-07-15 22:33:16 -0500

    save changes made  with help from classmates

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,18 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
+
+const dbData = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'tracker_db',
+    password: 'cody'
+},
+console.log('connected to tracker db')
+);
+
+dbData.connect(function (err) {
+    if(err) throw err
+});
+
+module.exports = dbData;
 
 
