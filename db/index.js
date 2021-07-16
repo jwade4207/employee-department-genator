1b2da9ce068c4223aa48b2efbecf6c4b01cae9d7
jwade4207 <jwade4207@yahoo.com>
2021-07-15 22:34:37 -0500

    save with help from classmates

--- db/index.js
+++ db/index.js
@@ -1,1 +1,34 @@
-require("console.table")
+const connection = require('./connection')
+
+class DB { 
+    constructor(connection) {
+        this.connection = connection 
+    }
+    seeAllDepartments() {
+        return this.connection.promise().query(`SELECT * FROM department`)
+    };
+    seeAllRoles() {
+        return this.connection.promise().query(`SELECT * FROM roles`)
+    };
+    selectAllEmployees() {
+        return this.connection.promise().query(`SELECT 
+    
+        employee.id, employee.first_name, employee.last_name, roles.title AS job_title, department.name AS department, roles.salary
+    
+        FROM employee
+        
+        JOIN roles ON employee.role_id = roles.id
+        
+        JOIN department ON roles.department_id = department.id;`)
+    };
+    createDepartment(department) {
+        return this.connection.promise().query(`INSERT INTO department SET ?`, department)
+    };   
+    createRole(roles) {
+        return this.connection.promise().query(`INSERT INTO roles SET ?, ?, ?`, roles)
+    };
+    createEmployee(employee) {
+        return this.connection.promise().query(`INSERT INTO employee SET ?, ?, ?, ?`, employee)
+    }
+}
+module.exports = new DB(connection)
